0162446c2b28ff6bbaedf12ae60a2679020536df
VATSALPANCHANIJ <reactdeveloperreactdeveloper3010@gmail.com>
2023-11-02 14:53:21 +0530

    shop_page_add filter

--- src/Component-1.6/Shop.jsx
+++ src/Component-1.6/Shop.jsx
@@ -7,37 +7,72 @@ const Shop = () => {
     const [loading, setLoading] = useState(true);
     const [allProducts, setAllProducts] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
+    const [category, setCategory] = useState([]);
+    const [product, setProduct] = useState();
+
     const Fetch_AllProducts = async () => {
         try {
             const res = await axios.get("http://localhost:3030/AllProduct_show_shop_page")
             setAllProducts(res.data);
-            console.log(res.data);
+
         } catch (error) {
             console.log(error);
         }
-
     }
+
+    //show 4 type of products veg, furits, etc...
+    const allcatogory = () => {
+        axios.get(`http://localhost:3030/catogery`)
+            .then((response) => {
+                setCategory(response.data);
+                console.log(response.data);
+            }).catch((error) => {
+                console.log(error);
+                return false;
+            })
+    }
+    const categoryFilter = (catogerydata) => {
+        console.log(catogerydata);
+        if (catogerydata === "all") {
+            Fetch_AllProducts();
+            console.log(allProducts);
+        } else {
+            axios.get(`http://localhost:3030/AllProduct_show_shop_page?catogery=${catogerydata}`).then((response) => {
+                setAllProducts(response.data);
+                console.log(response.data)
+            }).catch((error) => {
+                console.log(error);
+                return false;
+            })
+        }
+    }
+
+
+
+    // pagination code for
     const recordsPerPage = 12;
     const lastIndex = currentPage * recordsPerPage;
     const firstIndex = lastIndex - recordsPerPage;
-
     // this is a data showing that time use
     const currentProducts = allProducts.slice(firstIndex, lastIndex);
 
     const npage = Math.ceil(allProducts.length / recordsPerPage);
+    // pagee = 
     const numbers = [...Array(npage + 1).keys()].slice(1)
-
     function prePage() {
         if (firstIndex > 0) {
             setCurrentPage(currentPage - 1);
         }
     }
-
     function changeCPage(id) {
         setCurrentPage(id);
     }
+
+
     useEffect(() => {
         Fetch_AllProducts();
+        categoryFilter();
+        allcatogory()
         // Simulate an asynchronous action (e.g., fetching data from an API)
         setTimeout(() => {
             setLoading(false);
@@ -56,59 +91,27 @@ const Shop = () => {
                             <div className="row" >
                                 <div className="col-lg-3 col-md-4 col-12 ">
                                     <div className="all-filter">
-                                        <div className="categories-page-filter " style={{ position: 'fixed', marginTop: '50px' }}>
-                                            <h4 className="filter-title">Categories</h4>
+                                        <div className="categories-page-filter " >
+                                            <h4 class="filter-title">Categories</h4>
                                             <a href="#category-filter" data-bs-toggle="collapse" className="filter-link"><span>Categories </span><i className="fa fa-angle-down" /></a>
                                             <ul className="all-option collapse" id="category-filter">
-                                                <li className="grid-list-option">
-                                                    <input type="checkbox" />
-                                                    <a href="javascript:void(0)">Featured product</a>
-                                                </li>
-                                                <li className="grid-list-option">
-                                                    <input type="checkbox" />
-                                                    <a href="javascript:void(0)">Fresh fruits</a>
-                                                </li>
-                                                <li className="grid-list-option">
-                                                    <input type="checkbox" />
-                                                    <a href="javascript:void(0)">Fresh vegetable</a>
-                                                </li>
-                                                <li className="grid-list-option">
-                                                    <input type="checkbox" />
-                                                    <a href="javascript:void(0)">Organic juice</a>
-                                                </li>
-                                                <li className="grid-list-option">
-                                                    <input type="checkbox" />
-                                                    <a href="javascript:void(0)">Special product</a>
-                                                </li>
-                                                <li className="grid-list-option">
-                                                    <input type="checkbox" />
-                                                    <a href="javascript:void(0)">All products</a>
+                                                {category.map((val) => (
+                                                    <li className="Product_name"  key={val.catogery_name} onClick={() => categoryFilter(val.catogery_name)}dgmgkmsgmskgmlsgml>
+                                                        {/* <input type="checkbox" onClick={() => categoryFilter(val.catogery_name)} /> */}
+                                                        <a style={{ cursor: 'pointer' }} onClick={() => categoryFilter(val.catogery_name)}>{val.catogery_name}</a>
+                                                    </li>
+                                                ))}
+                                                <li onClick={() => categoryFilter('all')} style={{ width: '100%' }} type="button" className="Product_name">
+                                                    All
                                                 </li>
 
                                             </ul>
+
                                         </div>
-                                        {/* <div className="price-filter">
-                                    <h4 className="filter-title">Filter by price</h4>
-                                    <a href="#price-filter" data-bs-toggle="collapse" className="filter-link"><span>Filter by price </span><i className="fa fa-angle-down" /></a>
-                                    <ul className="all-price collapse" id="price-filter">
-                                        <price-range className="price-range">
-                                  
-                                            <div className="price-input-group group-input">
-                                                <div className="price-range-input input-prefix">
-                                                    <label className="input-prefix-label">From</label>
-                                                    <span className="input-prefix-value">$ <span id="demo1" /></span>
-                                                </div>
-                                                <span className="price-range-delimeter">-</span>
-                                                <div className="price-range-input input-prefix">
-                                                    <label className="input-prefix-label">To</label>
-                                                    <span className="input-prefix-value">$ <span id="demo2" /></span>
-                                                </div>
-                                            </div>
-                                        </price-range>
-                                    </ul>
-                                </div> */}
                                     </div>
+
                                 </div>
+
                                 <div className="col-lg-9 col-md-8 col-12">
                                     <div className="grid-list-banner" style={{ backgroundImage: 'url(image/slider17.jpg)' }}>
                                         <div className="grid-banner-content">
@@ -163,15 +166,15 @@ const Shop = () => {
                                         </div>
                                     </div>
                                     <nav className="mt-5">
-                                        <h6 className="tex-center col-12 d-flex justify-content-center mb-3" style={{color:'gray'}}>Showing 1 - 12 Product 3 Result More..</h6>
+                                        <h6 className="tex-center col-12 d-flex justify-content-center mb-3" style={{ color: 'gray' }}>Showing 1 - 12 Product 3 Result More..</h6>
                                         <ul className="pagination justify-content-center">
-                                           
+
                                             {numbers.map((n, i) => (
                                                 <li className={`page-link ${currentPage === n ? 'active' : ''}`} key={i}>
                                                     <a href="#" className='page-link' onClick={() => changeCPage(n)}>{n}</a>
                                                 </li>
                                             ))}
-                                           
+
                                         </ul>
                                     </nav>
                                 </div>
